c20168608d719b8b65293e256aaf10ebbc1b9bf1
swl <swl@inam>
2017-01-18 11:13:47 +0700

    hhh

--- modules/report/views/hosxpreport/personpttype.php
+++ modules/report/views/hosxpreport/personpttype.php
@@ -4,11 +4,56 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\data\ArrayDataProvider;
 use kartik\grid\GridView;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+?>
+<?php 
+    $form = ActiveForm::begin(['method'=>'get',
+        'action'=> Url::to(['hosxpreport/personpttype'])
+        ])
 ?>
 <div class="well">
-    
+    <div class="row">
+        <div class="col-xs-3 col-sm-3 col-md-3">
+             <?php
+        echo yii\jui\DatePicker::widget([
+            'name' => 'date1',
+            'value' => $date1,
+            'language' => 'th',
+            'dateFormat' => 'yyyy-MM-dd',
+            'clientOptions' => [
+                'changeMonth' => true,
+                'changeYear' => true,
+            ],
+            'options'=>[
+                'class'=>'form-control'
+            ],
+        ]);
+        ?>        
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-3">
+             <?php
+        echo yii\jui\DatePicker::widget([
+            'name' => 'date2',
+            'value' => $date2,
+            'language' => 'th',
+            'dateFormat' => 'yyyy-MM-dd',
+            'clientOptions' => [
+                'changeMonth' => true,
+                'changeYear' => true,
+            ],
+            'options'=>[
+                'class'=>'form-control'
+            ],
+        ]);
+        ?>        
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-3">
+            <button class="btn btn-warning"> ประมวลผล</button>
+        </div>        
+    </div>
 </div>
-
+<?php ActiveForm::end()?>
 <?php
 
 $gridColumns = [
